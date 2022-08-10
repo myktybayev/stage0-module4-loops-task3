24c080a33fea5a6c9df2175790f5e0328d49f375
Mykty <baha@Bahas-MacBook-Pro.local>
2022-08-10 19:31:43 +0600

    taks3 for loops

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,25 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int max = second, store = 0;
+
+        if(first > second){
+            max = first;
+        }
+
+        for(int i = 2; i <= max; i++){
+            if(first%i==0 && second%i==0){
+                store = i;
+            }
+        }
+        System.out.println(store);
     }
 }
+
+/*
+Algorithm to Find GCD
+Declare two variables, say x and y.
+Run a loop for x and y from 1 to max of x and y.
+Check that the number divides both (x and y) numbers completely or not. If divides completely store it in a variable.
+Divide the stored number.
+ */
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+        //[ 9 + 99 + 999 + 9999 ... n]
+        String nines = "";
+        for(int i = 0; i < lengthOfLastNumber; i++){
+            nines += "9";
+        }
 
+        int ninesInt = (!nines.equals(""))?Integer.parseInt(nines):0;
+        int sum = 0;
+        for(int j = 1; j < ninesInt; ninesInt /= 10){
+            sum += ninesInt;
+        }
+
+        System.out.println(sum);
     }
 }
